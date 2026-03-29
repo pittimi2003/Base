6e622e790198e7b7f4de8f9ece0d941287eb0c0e
pittimi2003 <94652956+pittimi2003@users.noreply.github.com>
2026-03-28 20:59:56 -0600

    Fix official server/wasm templates runtime compatibility

--- src/MachSoft.Template.Official.Wasm/template-content/Program.cs
+++ src/MachSoft.Template.Official.Wasm/template-content/Program.cs
@@ -1,4 +1,3 @@
-using MachSoft.Template.Core.Extensions;
 using TemplateRootNamespace;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -11,14 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(_ => new HttpClient
 {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-});
-
-builder.Services.AddMachSoftTemplateCore(options =>
-{
-    options.ApplicationName = typeof(App).Assembly.GetName().Name ?? "TemplateRootNamespace";
-    options.CompanyName = builder.Configuration["MachSoft:CompanyName"] ?? "TemplateCompanyName";
-    options.SupportEmail = builder.Configuration["MachSoft:SupportEmail"] ?? "platform@your-company.example";
-    options.DocumentationUrl = builder.Configuration["MachSoft:DocumentationUrl"] ?? "https://your-documentation-url";
 });
 
 await builder.Build().RunAsync();
