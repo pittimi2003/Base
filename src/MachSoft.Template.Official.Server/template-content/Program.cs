4671478dd253f5770a965565842322728eb1bc1b
pittimi2003 <94652956+pittimi2003@users.noreply.github.com>
2026-03-28 21:00:14 -0600

    Fix official server/wasm templates runtime compatibility (#125)

--- src/MachSoft.Template.Official.Server/template-content/Program.cs
+++ src/MachSoft.Template.Official.Server/template-content/Program.cs
@@ -1,4 +1,3 @@
-using MachSoft.Template.Core.Extensions;
 using TemplateRootNamespace.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,14 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services
     .AddRazorComponents()
     .AddInteractiveServerComponents();
-
-builder.Services.AddMachSoftTemplateCore(options =>
-{
-    options.ApplicationName = builder.Environment.ApplicationName;
-    options.CompanyName = builder.Configuration["MachSoft:CompanyName"] ?? "TemplateCompanyName";
-    options.SupportEmail = builder.Configuration["MachSoft:SupportEmail"] ?? "platform@your-company.example";
-    options.DocumentationUrl = builder.Configuration["MachSoft:DocumentationUrl"] ?? "https://your-documentation-url";
-});
 
 var app = builder.Build();
 
